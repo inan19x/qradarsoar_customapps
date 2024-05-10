474884c056725802c1b0829a9afa085de55dd869
Ade Ismail Isnan <61326551+inan19x@users.noreply.github.com>
2024-05-10 12:43:12 +0700

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,32 +1,13 @@
 import resilient
-import re
-import json 
 import logging 
-import smtplib
-import ssl
-import openpyxl
-import os
 import resilient_lib
-import time
-import shutil
-import email
-import email.header
-import email.mime.multipart
-
-from datetime import datetime
-from xml.dom import minidom
-
-from email import encoders
-from email.mime.base import MIMEBase
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
 
 from circuits.core.handlers import handler
 from resilient_circuits.actions_component import ResilientComponent, ActionMessage
 
 logger = logging.getLogger(__name__)
 
-class EscalationEmailProcessor(ResilientComponent):
+class LogHelloWorld(ResilientComponent):
 	# Subscribe to the Action Module message destination named 'hello_world'
 	channel = 'actions.hello_world'
 
